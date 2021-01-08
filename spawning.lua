db173465374738956056b6254536779c04bbe147
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2021-01-08 05:37:03 -0500

    Update spawning.lua

--- spawning.lua
+++ spawning.lua
@@ -3,9 +3,9 @@ local mob_list = {
     brachiosaurus = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass','default:river_water_source'}},
     carnotaurus = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass'}},
     dire_wolf = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass','default:dirt_with_rainforest_litter'}},
-    dunkleosteus = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass'}},
+    dunkleosteus = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:water_source'}},
     elasmotherium = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass'}}, --unicorn!
-    mammoth = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass','default:snowblock','default:ice','default:dirt_with_snow'}},
+    mammoth = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass','default:snowblock','default:ice','default:dirt_with_snow','default:permafrost_with_stones'}},
     mosasaurus = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:water_source'}},
     plesiosaurus = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:water_source'}},
     procoptodon = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass'}},
@@ -21,7 +21,7 @@ local mob_list = {
     velociraptor = {intrvl = 50, chance = 0.1, reduction = 0, near = {'default:dirt_with_grass','default:dirt_with_dry_grass','default:dry_dirt_with_dry_grass','default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
 }
 -- after defining these mobs, we can add mod dependant materials to the near array like this:
-if minetest.get_modpath('ethereal')
+if minetest.get_modpath('ethereal') then
     table.insert(mob_list.tyrannosaurus.near, 'ethereal:mushroom_dirt')
 end
 
@@ -33,7 +33,7 @@ for mob, def in ipairs(mob_list) do
             -- either search for a single matching node in radius or find all nodes that match in an area
             local pos1 = {x = spawnpos.x + radius, y = spawnpos.y + radius, z = spawnpos.z + radius}
             local pos2 = {x = spawnpos.x - radius, y = spawnpos.y - radius, z = spawnpos.z - radius}
-            if minetest.find_nodes_in_area(pos1, pos2, def.near)
+            if minetest.find_nodes_in_area(pos1, pos2, def.near) then
             -- if minetest.find_node_near(spawnpos, radius, def.near, true)
                 minetest.add_entity(spawnpos, "paleotest:"..mob)
             end
