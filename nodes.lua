0b40ea472ee1e725e6b432a457b6179296edc50b
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2021-01-08 20:58:35 -0500

    Changed after_place_leaves to default.after_place_leaves

--- nodes.lua
+++ nodes.lua
@@ -448,7 +448,7 @@ minetest.register_node("paleotest:metasequoia_leaves", {
     },
     sounds = default.node_sound_leaves_defaults(),
 
-    after_place_node = after_place_leaves
+    after_place_node = default.after_place_leaves
 })
 
 local function grow_sapling(pos)
