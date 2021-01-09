1bb15fb949f2e175042af3de6052bf7a86fe82fb
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2021-01-08 20:44:17 -0500

    Adjusted formatting, rearranged logic, cleaned up

--- spawning.lua
+++ spawning.lua
@@ -1,84 +1,75 @@
 minetest.register_on_mods_loaded(function()
-local allbiomes = minetest.registered_biomes
-local oceanbiomes = {}
-local shoresbiomes = {}
-local forestbiomes = {}
-local otherbiomes = {}
-local desertbiomes = {}
-local highlandbiomes = {}
-for biome, entry in ipairs(allbiomes) do
-    if string.find(biome, 'ocean') then
-        table.insert(oceanbiomes, biome)
-    elseif string.find(biome, 'shore') or string.find(biome, 'dune') then
-        table.insert(shoresbiomes, biome)
-    elseif string.find(biome, 'highland') then
-        table.insert(highlandbiomes, biome)
-    elseif string.find(biome, 'under') then
-        -- skip, no mobs spawn here
-    elseif string.find(biome, 'forest') then
-        table.insert(forestbiomes, biome)
-    elseif string.find(biome, 'desert') then
-        table.insert(desertbiomes, biome)
-    else
-        table.insert(otherbiomes)
-    end
-end
-local mob_list = {
-    -- create item for every mob with fine tuned settings here
-    brachiosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = {unpack(shoresbiomes), unpack(otherbiomes)}, near = {'default:dirt_with_grass','default:river_water_source'}},
-    carnotaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    dire_wolf = {intrvl = 5, chance = 0.7, reduction = 0, biomes = forestbiomes, near = {'default:dirt_with_grass','default:dirt_with_rainforest_litter'}},
-    dunkleosteus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = oceanbiomes, near = {'default:water_source'}},
-    elasmotherium = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}}, --unicorn!
-    mammoth = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass','default:snowblock','default:ice','default:dirt_with_snow','default:permafrost_with_stones'}},
-    mosasaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = oceanbiomes, near = {'default:water_source'}},
-    plesiosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = oceanbiomes, near = {'default:water_source'}},
-    procoptodon = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    pteranodon = {intrvl = 5, chance = 0.5, reduction = 0, biomes = highlandbiomes, near = {'default:dirt_with_grass'}},
-    quetzalcoatlus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = highlandbiomes, near = {'default:dirt_with_grass'}},
-    sarcosuchus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = shoresbiomes, near = {'default:river_water_source','default:sand'}},
-    smilodon = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    spinosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    stegosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    thylacoleo = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    triceratops = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    tyrannosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
-    velociraptor = {intrvl = 5, chance = 0.7, reduction = 0, biomes = forestbiomes, near = {'default:dirt_with_grass','default:dirt_with_dry_grass','default:dry_dirt_with_dry_grass','default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
-}
--- after defining these mobs, we can add mod dependant materials to the near array like this:
-if minetest.get_modpath('ethereal') then
-    table.insert(mob_list.tyrannosaurus.biomes, 'mushroom')
-end
-
-local function has_value (tab, val)
-    for index, value in ipairs(tab) do
-        if value == val then
-            return true
+    local allbiomes = minetest.registered_biomes
+    local oceanbiomes = {}
+    local shoresbiomes = {}
+    local forestbiomes = {}
+    local otherbiomes = {}
+    local desertbiomes = {}
+    local highlandbiomes = {}
+    for biome, entry in ipairs(allbiomes) do
+        if string.find(biome, 'ocean') then
+            table.insert(oceanbiomes, biome)
+        elseif string.find(biome, 'shore') or string.find(biome, 'dune') then
+            table.insert(shoresbiomes, biome)
+        elseif string.find(biome, 'highland') then
+            table.insert(highlandbiomes, biome)
+        elseif string.find(biome, 'under') then
+            -- skip, no mobs spawn here
+        elseif string.find(biome, 'forest') then
+            table.insert(forestbiomes, biome)
+        elseif string.find(biome, 'desert') then
+            table.insert(desertbiomes, biome)
+        else
+            table.insert(otherbiomes)
         end
     end
+    local mob_list = {
+        -- create item for every mob with fine tuned settings here
+        brachiosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = {unpack(shoresbiomes), unpack(otherbiomes)}, near = {'default:dirt_with_grass','default:river_water_source'}},
+        carnotaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        dire_wolf = {intrvl = 5, chance = 0.7, reduction = 0, biomes = forestbiomes, near = {'default:dirt_with_grass','default:dirt_with_rainforest_litter'}},
+        dunkleosteus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = oceanbiomes, near = {'default:water_source'}},
+        elasmotherium = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}}, --unicorn!
+        mammoth = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass','default:snowblock','default:ice','default:dirt_with_snow','default:permafrost_with_stones'}},
+        mosasaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = oceanbiomes, near = {'default:water_source'}},
+        plesiosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = oceanbiomes, near = {'default:water_source'}},
+        procoptodon = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        pteranodon = {intrvl = 5, chance = 0.5, reduction = 0, biomes = highlandbiomes, near = {'default:dirt_with_grass'}},
+        quetzalcoatlus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = highlandbiomes, near = {'default:dirt_with_grass'}},
+        sarcosuchus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = shoresbiomes, near = {'default:river_water_source','default:sand'}},
+        smilodon = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        spinosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        stegosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        thylacoleo = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        triceratops = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        tyrannosaurus = {intrvl = 5, chance = 0.5, reduction = 0, biomes = otherbiomes, near = {'default:dirt_with_grass'}},
+        velociraptor = {intrvl = 5, chance = 0.7, reduction = 0, biomes = forestbiomes, near = {'default:dirt_with_grass','default:dirt_with_dry_grass','default:dry_dirt_with_dry_grass','default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
+    }
+    -- after defining these mobs, we can add mod dependant materials to the near array like this:
+    -- if minetest.get_modpath('ethereal') then
+        -- table.insert(mob_list.tyrannosaurus.biomes, 'mushroom')
+    -- end
 
-    return false
-end
-
-local radius = 32 -- I expect the radius to be the same for every mob, if not we can add it as an item in the mob definitions array above, which would be accessed below as def.radius
-for mob, def in ipairs(mob_list) do
-    minetest.register_globalstep(function(dtime)
-        local spawnpos = mobkit.get_spawn_pos_abr(dtime, def.intrvl, radius, def.chance, def.reduction)
-        if spawnpos then
-            -- either search for a single matching node in radius or find all nodes that match in an area
-            -- local pos1 = {x = spawnpos.x + radius, y = spawnpos.y + radius, z = spawnpos.z + radius}
-            -- local pos2 = {x = spawnpos.x - radius, y = spawnpos.y - radius, z = spawnpos.z - radius}
-            -- if minetest.find_nodes_in_area(pos1, pos2, def.near) then
-            -- if minetest.find_node_near(spawnpos, radius, def.near, true)
-            local biome = minetest.get_biome_data(spawnpos)
-            local allowed_biomes = {}
-            for i, biome_name in ipair(def.biomes) do
-                allowed_biomes[i] = minetest.get_biome_id(biome_name)
+    local function has_value (tab, val)
+        for index, value in ipairs(tab) do
+            if value == val then
+                return true
             end
-            if has_value(allowed_biomes, biome.id) then
-                minetest.add_entity(spawnpos, "paleotest:"..mob)
+        end
+
+        return false
+    end
+
+    local radius = 32 -- I expect the radius to be the same for every mob, if not we can add it as an item in the mob definitions array above, which would be accessed below as def.radius
+    minetest.register_globalstep(function(dtime)
+        for mob, def in ipairs(mob_list) do
+            local spawnpos = mobkit.get_spawn_pos_abr(dtime, def.intrvl, radius, def.chance, def.reduction)
+            if spawnpos then
+                local biome = minetest.get_biome_data(spawnpos)
+                if has_value(def.biomes, minetest.get_biome_name(biome.id)) then
+                    minetest.add_entity(spawnpos, "paleotest:"..mob)
+                end
             end
         end
     end)
-end
 end)
