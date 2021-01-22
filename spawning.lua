368c7cfbce38a1c103ea15787e398ea183a5f905
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2021-01-21 21:01:56 -0500

    Adjusted spawn settings, adding plants

--- spawning.lua
+++ spawning.lua
@@ -1,5 +1,6 @@
 paleotest.spawn_rate = minetest.settings:get('spawn_rate')
 paleotest.spawn_chance = minetest.settings:get('spawn_chance') -- decimal value ranging from 0 to 2
+
 minetest.register_on_mapgen_init(function()
     local allbiomes = {}
     local oceanbiomes = {}
@@ -9,6 +10,7 @@ minetest.register_on_mapgen_init(function()
     local desertbiomes = {}
     local highlandbiomes = {}
     local swampbiomes = {}
+    local savannabiomes = {}
     for biome, entry in pairs(minetest.registered_biomes) do
         table.insert(allbiomes, biome)
         if string.find(biome, 'ocean') then
@@ -23,35 +25,54 @@ minetest.register_on_mapgen_init(function()
             -- skip, no mobs spawn here
         elseif string.find(biome, 'forest') then
             table.insert(forestbiomes, biome)
+        elseif string.find(biome, 'savanna') then
+            table.insert(savannabiomes, biome)
         elseif string.find(biome, 'desert') then
             table.insert(desertbiomes, biome)
         else
             table.insert(otherbiomes, biome)
         end
     end
+    minetest.register_decoration({
+        deco_type = "simple",
+        decoration = "paleotest:cycad_3",
+        place_on = "group:soil",
+        fill_ratio = 0.3,
+        biomes = {unpack(highlandbiomes), unpack(swampbiomes), unpack(forestbiomes), unpack(desertbiomes), unpack(savannabiomes), unpack(otherbiomes)},
+    })
+    minetest.register_decoration({
+        deco_type = "simple",
+        decoration = "paleotest:horsetail_3",
+        place_on = {"group:sand","group:soil"},
+        fill_ratio = 0.3,
+        biomes = {unpack(shoresbiomes), unpack(swampbiomes)},
+    })
     local mob_list = {
         -- create item for every mob with fine-tuned settings here
-        brachiosaurus = {intrvl = 5, chance = 0.5, group = 3, biomes = {unpack(shoresbiomes), unpack(swampbiomes), unpack(otherbiomes)}, nodes = {'group:soil','group:water'}},
-        carnotaurus = {intrvl = 5, chance = 0.5, group = 3, biomes = otherbiomes, nodes = {'group:soil'}},
-        dire_wolf = {intrvl = 5, chance = 0.5, group = 10, biomes = forestbiomes, nodes = {'default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
-        dunkleosteus = {intrvl = 5, chance = 0.5, group = 4, biomes = oceanbiomes, nodes = {'group:water'}},
-        elasmotherium = {intrvl = 5, chance = 0.5, group = 6, biomes = otherbiomes, nodes = {'group:soil'}}, --unicorn!
-        mammoth = {intrvl = 5, chance = 0.5, group = 4, biomes = otherbiomes, nodes = {'group:soil','group:snowy','default:ice','default:permafrost_with_stones'}},
-        mosasaurus = {intrvl = 5, chance = 0.5, group = 2, biomes = oceanbiomes, nodes = {'group:water'}},
-        plesiosaurus = {intrvl = 5, chance = 0.5, group = 2, biomes = oceanbiomes, nodes = {'group:water'}},
-        procoptodon = {intrvl = 5, chance = 0.5, group = 6, biomes = otherbiomes, nodes = {'group:soil'}},
-        pteranodon = {intrvl = 5, chance = 0.5, group = 6, biomes = {unpack(highlandbiomes), unpack(otherbiomes)}, nodes = {'group:soil', 'default:permafrost_with_stones'}},
-        quetzalcoatlus = {intrvl = 5, chance = 0.5, group = 6, biomes = {unpack(highlandbiomes), unpack(otherbiomes)}, nodes = {'group:soil', 'default:permafrost_with_stones'}},
-        sarcosuchus = {intrvl = 5, chance = 0.5, group = 3, biomes = {unpack(shoresbiomes), unpack(swampbiomes)}, nodes = {'group:water','group:sand'}},
-        smilodon = {intrvl = 5, chance = 0.5, group = 2, biomes = otherbiomes, nodes = {'group:soil', 'group:snowy', 'default:ice', 'default:permafrost_with_stones'}},
-        spinosaurus = {intrvl = 5, chance = 0.5, group = 3, biomes = {unpack(shoresbiomes), unpack(swampbiomes)}, nodes = {'group:sand', 'group:water'}},
-        stegosaurus = {intrvl = 5, chance = 0.5, group = 6, biomes = otherbiomes, nodes = {'group:soil'}},
-        thylacoleo = {intrvl = 5, chance = 0.5, group = 3, biomes = otherbiomes, nodes = {'group:soil'}},
-        triceratops = {intrvl = 5, chance = 0.5, group = 6, biomes = otherbiomes, nodes = {'group:soil'}},
-        tyrannosaurus = {intrvl = 5, chance = 0.5, group = 3, biomes = otherbiomes, nodes = {'group:soil'}},
-        velociraptor = {intrvl = 5, chance = 0.5, group = 10, biomes = forestbiomes, nodes = {'default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
+        -- sea monsters
+        dunkleosteus = {intrvl = 30, chance = 0.8, group = 4, biomes = oceanbiomes, nodes = {'group:water'}},
+        mosasaurus = {intrvl = 30, chance = 0.8, group = 4, biomes = oceanbiomes, nodes = {'group:water'}},
+        plesiosaurus = {intrvl = 30, chance = 0.8, group = 4, biomes = oceanbiomes, nodes = {'group:water'}},
+        -- reptiles
+        brachiosaurus = {intrvl = 30, chance = 0.5, group = 3, biomes = {unpack(shoresbiomes), unpack(swampbiomes)}, nodes = {'group:sand','group:soil','group:water'}},
+        carnotaurus = {intrvl = 30, chance = 0.4, group = 3, biomes = otherbiomes, nodes = {'group:soil'}},
+        pteranodon = {intrvl = 30, chance = 0.8, group = 6, biomes = {unpack(highlandbiomes), unpack(otherbiomes)}, nodes = {'group:soil','default:permafrost_with_stones','air'}},
+        quetzalcoatlus = {intrvl = 30, chance = 0.8, group = 6, biomes = {unpack(highlandbiomes), unpack(otherbiomes)}, nodes = {'group:soil','default:permafrost_with_stones','air'}},
+        sarcosuchus = {intrvl = 30, chance = 0.7, group = 3, biomes = {unpack(shoresbiomes), unpack(swampbiomes)}, nodes = {'group:water','group:sand'}},
+        spinosaurus = {intrvl = 30, chance = 0.5, group = 3, biomes = {unpack(shoresbiomes), unpack(swampbiomes)}, nodes = {'group:water','group:sand'}},
+        stegosaurus = {intrvl = 30, chance = 0.5, group = 6, biomes = otherbiomes, nodes = {'group:soil'}},
+        triceratops = {intrvl = 30, chance = 0.6, group = 6, biomes = otherbiomes, nodes = {'group:soil'}},
+        tyrannosaurus = {intrvl = 30, chance = 0.4, group = 3, biomes = otherbiomes, nodes = {'group:soil'}},
+        velociraptor = {intrvl = 30, chance = 0.6, group = 10, biomes = forestbiomes, nodes = {'default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
+        -- mammals
+        dire_wolf = {intrvl = 30, chance = 0.8, group = 10, biomes = forestbiomes, nodes = {'default:dirt_with_coniferous_litter','default:dirt_with_rainforest_litter'}},
+        elasmotherium = {intrvl = 30, chance = 0.6, group = 6, biomes = otherbiomes, nodes = {'group:soil'}}, --unicorn!
+        mammoth = {intrvl = 30, chance = 0.7, group = 4, biomes = otherbiomes, nodes = {'group:soil','group:snowy','default:ice','default:permafrost_with_stones'}},
+        procoptodon = {intrvl = 30, chance = 0.9, group = 6, biomes = savannabiomes, nodes = {'group:soil'}},
+        smilodon = {intrvl = 30, chance = 0.7, group = 3, biomes = otherbiomes, nodes = {'group:soil', 'group:snowy', 'default:ice', 'default:permafrost_with_stones'}},
+        thylacoleo = {intrvl = 30, chance = 0.9, group = 3, biomes = savannabiomes, nodes = {'group:soil'}},
     }
-    -- after defining these mobs, we can add mod dependant stuff to the near array like this:
+    -- after defining these mobs, we can add mod dependant stuff to the tables like this:
     -- if minetest.get_modpath('ethereal') then
         -- table.insert(mob_list.tyrannosaurus.nodes, 'ethereal:mushroom_dirt')
     -- end
